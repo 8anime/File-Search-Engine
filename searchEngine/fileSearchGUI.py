4a400b3ce4321d225503ab15f085d4d8af321ed0
8anime.to <philiprigha8@gmail.com>
2023-10-10 20:33:31 +0300

    Add save file functionality, remove filter functionality, update some comments and changed resultsDisplay to resultsDisplayView

--- searchEngine/fileSearchGUI.py
+++ searchEngine/fileSearchGUI.py
@@ -10,8 +10,11 @@ from PyQt5.QtWidgets import (
     QPushButton,
     QComboBox,
     QListWidget,
-    QListWidgetItem
+    QListWidgetItem,
+    QFileDialog,
+    QMessageBox
 )
+
 
 from csvSearch import searchCsvFile  # Import your search function from csvSearch module
 
@@ -19,7 +22,7 @@ from csvSearch import searchCsvFile  # Import your search function from csvSearc
 class CSVSearchApp(QMainWindow):
     """The CSVSearchApp is a PyQt5-based desktop application that allows users to search and query data from a CSV file 
     using a user-friendly graphical interface. It provides features for entering search keywords, specifying columns to 
-    search in, and displaying search results in a list format. Users can also sort and filter results to quickly find and 
+    search in and displaying search results in a list format. Users can also sort and save results to quickly find and 
     explore relevant data within the CSV file."""
     def __init__(self):
         super().__init__()
@@ -29,10 +32,10 @@ class CSVSearchApp(QMainWindow):
     def initializeUserInterface(self):
         """Build the user interface of the application"""
         self.setWindowTitle('CSV Search App')  # Window Title
-        self.setGeometry(450, 100, 400, 500)   # Set width and height of the window and where on the screen it will appear
+        self.setGeometry(450, 100, 500, 500)   # Set width and height of the window and where on the screen it will appear
 
-        # Determine the arrangement and alignment of the various widgets within the application's main window
-        # It is responsible for organizing the widgets in the main window in a vertical orientation from top to bottom
+        # Determine the arrangement and alignment of the various widgets within the application's main window.
+        # It is responsible for organizing the widgets in the main window in a vertical orientation from top to bottom.
         widgetLayout = QVBoxLayout()
 
         # Add keyword search box
@@ -43,7 +46,7 @@ class CSVSearchApp(QMainWindow):
 
         # Add column selection box
         self.columnInput = QLineEdit()                                      # Column input field
-        columnLabel = QLabel('Enter columns to search (comma-separated):')  # Column label
+        columnLabel = QLabel('Enter column to search:')  # Column label
         widgetLayout.addWidget(columnLabel)                                 # Add label to the layout
         widgetLayout.addWidget(self.columnInput)                            # Add input field to the layout 
 
@@ -52,30 +55,26 @@ class CSVSearchApp(QMainWindow):
         widgetLayout.addWidget(searchButton)  # Add search button to the layout
 
         # Create a QListWidget view to display search results
-        self.resultsDisplay = QListWidget()
-        widgetLayout.addWidget(self.resultsDisplay)  # Add widget in the layout
+        self.resultsDisplayView = QListWidget()
+        widgetLayout.addWidget(self.resultsDisplayView)  # Add widget in the layout
 
         # Create a dropdown menu that allows users to select sorting category from a list of options
         self.sortingDropDown = QComboBox()
-        self.sortingDropDown.addItems(['Ascending', 'Descending'])  # Sort search results in ascending and descending order
-        widgetLayout.addWidget(self.sortingDropDown)                # Add widget to the layout
+        self.sortingDropDown.addItems(['Ascending', 'Descending'])  # Sort search results in ascending or descending order
+        widgetLayout.addWidget(self.sortingDropDown)                # Add dropdown menu to the layout
 
-        # Add filter input field
-        self.filterInputField = QLineEdit()            # Input field
-        widgetLayout.addWidget(self.filterInputField)  # Add widget to the layout
-        
-        # Add filter button
-        filterButton = QPushButton('Filter')  # Filter button
-        widgetLayout.addWidget(filterButton)  # Add widget the layout
+        # Add export button
+        saveButton = QPushButton('Save')            # Save button
+        widgetLayout.addWidget(saveButton)          # Add save button to the layout
 
         # Connect the search button click event to the search function
         searchButton.clicked.connect(self.performSearch) 
 
-        # Connect the filter button click event to the handle filter function 
-        filterButton.clicked.connect(self.handleFilter)
-
-        # Connect sorting and filtering controls to functions
+        # Connect sorting control to function
         self.sortingDropDown.currentIndexChanged.connect(self.handleSorting)
+
+        # Connect export button click event to the exportResults function
+        saveButton.clicked.connect(self.saveResults)
 
         # Create and set up the main widget
         centralWidget = QWidget()
@@ -88,18 +87,18 @@ class CSVSearchApp(QMainWindow):
         searchKeyword = self.keywordInput.text().strip()   # Extract text from the keyword input search box
         columnsToSearch = self.columnInput.text()          # Extract text from the column input search box
 
-        results = searchCsvFile(searchKeyword, columnsToSearch)  # Store the results returned from the function in a variable
+        results = searchCsvFile(searchKeyword, columnsToSearch)  # Store the list returned from the function in a variable
 
-        self.resultsDisplay.clear()  # Removes any previously searched results before displaying the new results
+        self.resultsDisplayView.clear()  # Removes any previously searched results before displaying the new results
 
         if not results:  # Checks if result is being returned from the function
             item = QListWidgetItem("No matching results found.")  # This is displayed if no result is found
-            self.resultsDisplay.addItem(item)                     # Add the display message to the QListWidget view
+            self.resultsDisplayView.addItem(item)                 # Add the display message to the QListWidget view
         else:
             # Display each matching result in the QListWidget view
-            for result in results:                 # For every result stored in the results variable
-                item = QListWidgetItem(result)     # Create an item object for each result stored in the result variable
-                self.resultsDisplay.addItem(item)  # Add each item in the QListWidget view
+            for result in results:                     # Loop through the results(List) variable
+                item = QListWidgetItem(result)         # Create an item object for each result stored in the result variable
+                self.resultsDisplayView.addItem(item)  # Add each item in the QListWidget view
 
     def handleSorting(self):
         """Implement sorting logic based on the user's selection"""
@@ -107,7 +106,7 @@ class CSVSearchApp(QMainWindow):
         sortingOrder = self.sortingDropDown.currentText()  # Get text from the combo box
 
         # Get the current search results from the QListWidget view
-        currentResults = [self.resultsDisplay.item(i).text() for i in range(self.resultsDisplay.count())]
+        currentResults = [self.resultsDisplayView.item(i).text() for i in range(self.resultsDisplayView.count())]
 
         if sortingOrder == 'Ascending':           
             currentResults.sort()                 # Ascending by default
@@ -115,18 +114,39 @@ class CSVSearchApp(QMainWindow):
             currentResults.sort(reverse=True)     # 'reverse=True' makes it descending
 
         # Clear the current results in the QListWidget view to show sorting results
-        self.resultsDisplay.clear()
+        self.resultsDisplayView.clear()
 
         # Display the sorted results in the QListWidget view
-        for result in currentResults:             # For every result stored in the results variable 
-            item = QListWidgetItem(result)        # Create an item object for each result stored in the result variable 
-            self.resultsDisplay.addItem(item)     # Add each item in the QListWidget view
+        for result in currentResults:               # Loop through the currentResults(List) variable 
+            item = QListWidgetItem(result)          # Create an item object for each result stored in the currentResults variable 
+            self.resultsDisplayView.addItem(item)   # Add each item in the QListWidget view
 
-    def handleFilter(self):
-        filter_criteria = self.filterInputField.text()
-        # Implement filtering logic based on the user's input
-        # Update the displayed data to show filtered results
-        pass
+    def saveResults(self):
+        """Allows users to save and export the obtained results for future reference"""
+        # Get the current search results from the QListWidget view
+        currentResults = [self.resultsDisplayView.item(i).text() for i in range(self.resultsDisplayView.count())]
+
+        if not currentResults:  # Checks if results have been returned
+            return              # No results to save
+        
+        # Open a file dialog to let the user choose where to save the results
+        # Specify various options and flags that control the behaviour and appearance of the file dialog
+        options = QFileDialog.Options()  # Control how the file dialog behaves
+        # Display a file dialog for saving a file
+        filePath, _ = QFileDialog.getSaveFileName(
+            # Main window, Title of the file dialog, current directory, Display all files and files with .csv extension and pass the options variable
+            self, "Save Results", "", "CSV Files (*.csv);;All Files (*)", options=options
+        )
+
+        if filePath:  # Check if filePath exists
+            # Write the results to the selected file in CSV format
+            try:
+                # Write the results to the selected file in CSV format
+                with open(filePath, 'w', encoding='utf-8') as file:
+                    file.write('\n'.join(currentResults))
+                QMessageBox.information(self, "Success", "Results saved successfully.")
+            except Exception as e:
+                QMessageBox.critical(self, "Error", f"An error occurred while exporting results: {str(e)}")
 
 
 if __name__ == '__main__':
