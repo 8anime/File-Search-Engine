baae91ae4425d27c142103ccca665cf4649c2c9f
8anime.to <philiprigha8@gmail.com>
2023-10-10 10:56:00 +0300

    Updated searchCsvFile function to handle sorting of data

--- searchEngine/csvSearch.py
+++ searchEngine/csvSearch.py
@@ -2,11 +2,11 @@
 import csv
 import os
 
-def searchCsvFile(keyword, column):
+def searchCsvFile(keyword, column, sortingOrder='ascending'):
     """Search for data from the csv file"""
     fileLocation = os.path.join('csvFolder', 'anime.csv')
 
-    matchingData = []
+    matchingData = []  # Stores returned values from the csv
 
     with open(fileLocation, 'r', encoding='utf-8') as file:
         reader = csv.DictReader(file)
@@ -15,9 +15,16 @@ def searchCsvFile(keyword, column):
         if column not in reader.fieldnames:
             return matchingData  # Return an empty list if the column doesn't exist
 
-        for row in reader:
-            if keyword.lower() in row[column].lower():
-                matchingData.append(row[column])
+        for row in reader:  # 'row' is treated like a dictionary
+            # Check if a keyword value exists in the value returned from a row
+            if keyword.lower() in row[column].lower():  # 'column' is the key that retrieves a value(s) from the row(dictionary)
+                matchingData.append(row[column])        # Add the value to 'matchingData' list if a keyword value exists in it
+
+    # Apply sorting based on the sortingOrder parameter
+    if sortingOrder == 'ascending':
+        matchingData.sort()               # Sort in ascending order by default
+    elif sortingOrder == 'descending':
+        matchingData.sort(reverse=True)   # 'reverse=True' makes it sort in descending order
 
     return matchingData
 
