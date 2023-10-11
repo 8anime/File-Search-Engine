4d2cdcba87809a8afc8feffefc64c2602fbef40c
8anime.to <philiprigha8@gmail.com>
2023-10-11 10:12:49 +0300

    Refactor file to encapsulate data processing in a dedicated function

--- columnNames.py
+++ columnNames.py
@@ -9,23 +9,25 @@ import csv
 
 ROOTDIR = '.\\'
 FOLDER = 'csvFolder'
-FILE = 'anime.csv'
+FILE = 'anime.csv'  # update this part to your desired csv file
 
 # Define the file location of the CSV file to be read
 fileLocation = os.path.join(ROOTDIR, FOLDER, FILE)
 
-# Open the CSV file for reading with specified encoding
-with open(fileLocation, 'r', encoding='utf-8') as file:
-    # Create a CSV reader object that treats the first row as column names
-    reader = csv.DictReader(file)
+def csvReader():
+    """Read a csv file"""
+    # Open the CSV file for reading with specified encoding
+    with open(fileLocation, 'r', encoding='utf-8') as file:
+        # Create a CSV reader object that treats the first row as column names
+        reader = csv.DictReader(file)
+    
+        # Get the column names from the CSV file
+        columns = reader.fieldnames
+    
+        # Print the column names
+        print(columns)
 
-    # Get the column names from the CSV file
-    columns = reader.fieldnames
-
-    # Print the column names
-    print(columns)
-
-
+csvReader()
 
 
 
